836178cf1296e89a1bd03dbb1c53b433224997f1
laibaali85 <laibaali858452@gmail.com>
2024-09-25 07:38:58 -0700

    first commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>THIS IS A CONTACT US PAGE!</div>
+  )
+}
+
+export default contact
---
+++ app/footer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>THIS IS FOOTER PAGE!</div>
+  )
+}
+
+export default footer
